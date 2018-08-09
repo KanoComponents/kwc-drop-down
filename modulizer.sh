275b03e32ca8380aa2ad404654424e0c74afe2de
Radek Pazdera <radek@kano.me>
2018-08-09 10:17:53 +0200

    Bumping version

--- modulizer.sh
+++ modulizer.sh
@@ -1,6 +1,6 @@
 modulizer --out . \
 --import-style name \
 --npm-name @kano/kwc-drop-down \
---npm-version 3.0.0-beta.4 \
+--npm-version 3.0.0-beta.5 \
 --dependency-mapping kwc-icons,@kano/kwc-icons,3.0.0-beta.2 \
 --dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.2
